e3b22acb6821e84dbb38861b3e1efefdf37f1070
thomas.bugs <thomas.bugs@khomp.com>
2022-12-15 11:09:53 -0300

    Rede wifi configuravel

--- Plug.ino
+++ Plug.ino
@@ -4,6 +4,7 @@
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <ESP8266WebServer.h>
 
 #define LED_VERDE 2
 #define RESET_BUTTON 13 //5
@@ -17,13 +18,21 @@ int P2Status = 0;
 
 // Variáveis para acessar wi-fi
 
-// WIFI Casa
-const char* ssid = "Virus_cavalo_de_troia";
-const char* password = "putygrillputygrill280642";
+// Access Point
+const char* ssidAP = "ESP8266-PLUG-AP";
+const char* passwordAP = "plugappasswordesp12E";
 
-// Hotspot smartphone
-const char* ssidHot = "Oie";
-const char* passwordHot = "qqij8mh5shns6c5";
+// WiFi
+String ssid;
+String password;
+
+bool wificonnect = false;
+
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+ESP8266WebServer server(80);
 
 // Variáveis MQTT
 const char* mqtt_server = "broker.mqtt-dashboard.com";
@@ -37,18 +46,11 @@ char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
 void mqtt_callback(char* topic, byte* payload, unsigned int length){
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
   String topicString = topic;
   String msgcallback = "";
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
     msgcallback += (char)payload[i];
   }
-  Serial.println();
-  Serial.print("Mensagem: ");
-  Serial.println(msgcallback);
   if(topicString.equals("trab_ubi/sub_plug1")){
     if(msgcallback.equals("1")){
       digitalWrite(TOMADA_1, HIGH);
@@ -76,23 +78,15 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length){
 
 // mqtt Reconnect
 void reconnectmqtt() {                                                       
-  while (!client.connected()) {                                          
-    Serial.print("Aguardando conexão MQTT...");
+  while (!client.connected()) {
     String clientID = "ESP-01Client-";
     clientID += String(random(0xffff, HEX));
-    if (client.connect(clientID.c_str())) {                               
-      Serial.println("conectado");
-      Serial.print("Client ID: ");
-      Serial.println(clientID);
-      Serial.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");                                       
+    if (client.connect(clientID.c_str())) {                                      
       client.publish("pub_plug_connect_ubi", "Connected");    
       client.subscribe("sub_test_ubi");                
       client.subscribe("trab_ubi/sub_plug1");  
       client.subscribe("trab_ubi/sub_plug2");           
-    } else {                                                             
-      Serial.print("falhou, rc=");                                       
-      Serial.print(client.state());                                      
-      Serial.println(" tente novamente em 5s");                          
+    } else {                         
       delay(5000);                                                       
     }
   }
@@ -100,24 +94,14 @@ void reconnectmqtt() {
 
 // WiFi Management
 
-int connectWifi(int cred)
+int connectWifi()
 {
   WiFi.mode(WIFI_STA);
-  switch (cred)
-  {
-    case 0:
-      Serial.println(ssid);
-      WiFi.begin(ssid, password);
-      break;
-    case 1:
-      Serial.println(ssidHot);
-      WiFi.begin(ssidHot, passwordHot);
-      break;
-  }
+  WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     return 0;
     delay(1000);
-  }
+  }  
   return 1;
 }
 
@@ -131,71 +115,16 @@ void setup() {
   digitalWrite(TOMADA_1, LOW);
   digitalWrite(TOMADA_2, LOW);
 
-  Serial.begin(115200);
-  delay(50);
-  Serial.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-  Serial.println("Iniciando...");
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  WiFi.softAP(ssidAP, passwordAP);
+  delay(100);
 
-  int i = 0;
-  while( i < 10 ) // Tenta conectar as 2 redes descritas anteriormente 5 vezes cada
-  {
-    Serial.println("Tentando conexao a rede");
-    if(connectWifi( i % 2 ))
-    {
-      Serial.println("Conectado!");
-      Serial.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-      break;
-    }
-    else
-    {
-      Serial.println("Conexao falhou!");
-    }
-    i++;
-  }
-  if( i >= 10 )
-  {
-    Serial.println("Falha ao tentar conexão WiFi, reiniciando ESP!");
-    delay(5000);
-    ESP.restart();
-  }
+  server.on("/", handle_OnConnect);
+  server.on("/conectar", handle_Conectar);
 
-  // Porta padrao do ESP8266 para OTA eh 8266 - Voce pode mudar ser quiser, mas deixe indicado!
-  // ArduinoOTA.setPort(8266);
- 
-  // O Hostname padrao eh esp8266-[ChipID], mas voce pode mudar com essa funcao
-  // ArduinoOTA.setHostname("nome_do_meu_esp8266");
- 
-  // Nenhuma senha eh pedida, mas voce pode dar mais seguranca pedindo uma senha pra gravar
-  // ArduinoOTA.setPassword((const char *)"123");
- 
-  ArduinoOTA.onStart([]() {
-    Serial.println("Inicio...");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("nFim!");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progresso: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Erro [%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Autenticacao Falhou");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Falha no Inicio");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Falha na Conexao");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Falha na Recepcao");
-    else if (error == OTA_END_ERROR) Serial.println("Falha no Fim");
-  });
-  ArduinoOTA.begin();
-  Serial.println("Pronto");
-  Serial.print("Endereco IP: ");
-  Serial.println(WiFi.localIP());
+  server.begin();
   
   // FIM DAS CONFIGURACOES DO OTA
-
-  randomSeed(micros());
-
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(mqtt_callback);
 
   digitalWrite(LED_VERDE, HIGH);
   client.publish("trab_ubi/pub_plug1_status", P1Status ? "ON" : "OFF");
@@ -205,6 +134,11 @@ void setup() {
  
 void loop() {
   ArduinoOTA.handle(); // NAO RETIRAR ESTA LINHA!!!
+
+  if(!wificonnect){
+    server.handleClient();
+    return;
+  }
   
   if (!client.connected()) {                                             
     reconnectmqtt();
@@ -215,4 +149,85 @@ void loop() {
   
   
 
+}
+
+void handle_OnConnect() {
+
+  server.send(200, "text/html", HTMLWebServer());
+  
+}
+
+void handle_Conectar() {
+
+  ssid = server.arg("network");
+  password = server.arg("wifipassword");
+  delay(500);
+
+  if(connectWifi())
+  { 
+
+    wificonnect = true;
+    
+    randomSeed(micros());
+
+    client.setServer(mqtt_server, mqtt_port);
+    client.setCallback(mqtt_callback);
+
+    ArduinoOTA.begin();
+  }
+  else{
+    ESP.restart();
+  }
+}
+
+String HTMLWebServer() {
+
+ String htmlweb = "<!DOCTYPE html> <html>\n";
+
+ // Head
+ htmlweb += "<head>\n";
+ 
+ htmlweb += "<meta charset=\"UTF-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+ htmlweb += "<title>ESP-12E Smart Plug Configuration</title>\n";
+ 
+ htmlweb += "<style>\n";
+ htmlweb += "html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+ htmlweb += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+ htmlweb += ".button {display: block;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 20px;margin: 35px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+ htmlweb += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+ htmlweb += "</style>\n";
+ 
+ htmlweb += "</head>\n";
+
+ // Body
+ htmlweb += "<body>\n";
+ 
+ htmlweb += "<h1>Configuração de rede ESP-12E</h1>\n";
+
+ htmlweb += "<form action=\"/conectar\">\n";
+ 
+ htmlweb += "<p>Rede WiFi</p>\n";
+ htmlweb += "<select name=\"network\" required>\n";
+ htmlweb += "<option value=\"\" selected>Selecione</option>\n";
+ int NNetworks = WiFi.scanNetworks();
+ for(int it = 0 ; it < NNetworks ; it++)
+ {
+  htmlweb += "<option value=\"" + WiFi.SSID(it) + "\">" + WiFi.SSID(it) + "</option>";
+ }
+ htmlweb += "</select>\n";
+
+ htmlweb += "<p>Senha</p>\n";
+
+ htmlweb += "<input name=\"wifipassword\" type=\"password\" value>\n";
+
+ htmlweb += "<input class=\"button\" type=\"submit\" value=\"Conectar\">\n";
+
+ htmlweb += "</form>\n";
+ 
+ htmlweb += "</body>\n";
+
+ htmlweb += "</html>\n";
+
+ return htmlweb;
+ 
 }
